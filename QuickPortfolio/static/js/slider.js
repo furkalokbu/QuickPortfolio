030f45e18eb3a43eb26b298ed6cb0565b3f68446
Serhii <furkalokbu@gmail.com>
2021-05-16 12:34:20 +0300

    fix js

--- QuickPortfolio/static/js/slider.js
+++ QuickPortfolio/static/js/slider.js
@@ -1,50 +1,48 @@
-// window.addEventListener("DOMContentLoaded",() => {
-        /* Индекс слайда по умолчанию */
-        var slideIndex = 1;
-        showSlides(slideIndex);
-    
-        /* Функция увеличивает индекс на 1, показывает следующй слайд*/
-        function plusSlide() {
-        showSlides(slideIndex += 1);
-        }
-    
-        /* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
-        function minusSlide() {
-        showSlides(slideIndex -= 1);  
-        }
-    
-        /* Устанавливает текущий слайд */
-        function currentSlide(n) {
-        showSlides(slideIndex = n);
-        }
-    
-        /* Основная функция слайдера */
-        function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("item");
-        var dots = document.getElementsByClassName("slider-dots_item");
-        var text = document.getElementsByClassName("text_slider");
-    
-    
-        if (n > slides.length) {
-            slideIndex = 1;
-        }
-        if (n < 1) {
-            slideIndex = slides.length;
-        }
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-            text[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex - 1].style.display = "block";
-        text[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " active";
-        }
-    
-// });
+    // Ваш скрипт
 
+    /* Индекс слайда по умолчанию */
+    var slideIndex = 1;
+    showSlides(slideIndex);
 
-   
+    /* Функция увеличивает индекс на 1, показывает следующй слайд*/
+    function plusSlide() {
+    showSlides(slideIndex += 1);
+    }
+
+    /* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+    function minusSlide() {
+    showSlides(slideIndex -= 1);  
+    }
+
+    /* Устанавливает текущий слайд */
+    function currentSlide(n) {
+    showSlides(slideIndex = n);
+    }
+
+    /* Основная функция слайдера */
+    function showSlides(n) {
+    
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider-dots_item");
+    var text = document.getElementsByClassName("text_slider");
+
+    
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        text[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    text[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    }
