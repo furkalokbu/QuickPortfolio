ea98dc68337797d572c74d95c7d37ba85181d388
mechanic <furkalokbu@gmail.com>
2021-05-18 07:59:11 +0300

    permission

--- projects/api/views.py
+++ projects/api/views.py
@@ -1,18 +1,14 @@
 from rest_framework import generics, viewsets
 from ..models import Portfolio
 from .serializers import PortfolioSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .permissions import IsAuthorOrReadOnly
 
-
-# class PortfolioListView(generics.ListAPIView):
-#     queryset = Portfolio.objects.all()
-#     serializer_class = PortfolioSerializer
 
 class PortfolioListView(viewsets.ModelViewSet):
     queryset = Portfolio.objects.all()
     serializer_class = PortfolioSerializer
-    permission_classes = [IsAuthorOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
   
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
