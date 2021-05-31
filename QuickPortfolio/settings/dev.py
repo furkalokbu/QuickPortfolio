14f5af09521b7ce9b763749371fea9631cb1b17c
Serhii <furkalokbu@gmail.com>
2021-05-31 17:03:42 +0300

    debug

--- QuickPortfolio/settings/dev.py
+++ QuickPortfolio/settings/dev.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-f2qpd#g#3ttwenflk9r_26&rrty!+wn3$-7yg=21mv!1%q3bd9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
